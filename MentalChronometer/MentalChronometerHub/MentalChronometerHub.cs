9772811975409c2b3b522975d63b98c06e4e1369
Valery Jacobs <valeryjacobs@hotmail.com>
2017-04-07 00:27:38 +0200

    Init

---
+++ MentalChronometer/MentalChronometerHub/MentalChronometerHub.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MentalChronometerHub
+{
+    public class MentalChronometerHub : Hub
+    {
+        Dictionary<string, List<int>> DeviceRegister = new Dictionary<string, List<int>>();
+
+        public void Send(string name, string message)
+        {
+            // Call the broadcastMessage method to update clients.
+            Clients.All.broadcastMessage(name, message);
+        }
+
+        public void Eenofanderenaam(string deviceId, string payload)
+        {
+            // Call the broadcastMessage method to update clients.
+            Clients.All.toDevice(payload);
+        }
+
+
+
+        public void Register(string deviceId)
+        {
+            if (!DeviceRegister.ContainsKey(deviceId))
+                DeviceRegister.Add(deviceId, null);
+            else
+                DeviceRegister[deviceId] = null;
+        }
+    }   
+}
